21cb5b04a9ee8663beb69cbcbcd55dff5f37bf03
Emily Qin <eyq3@duke.edu>
2020-12-06 20:55:12 -0600

    deal hands

--- yusef.py
+++ yusef.py
@@ -27,8 +27,19 @@ def deal_top_card(deck:list) -> str:
     return card
 
 def deal_hands(deck:list,numHands:int,sizeHand:int) -> list:
-    
+    hands=[]
+    while numHands>0 and len(deck)>0:
+        hand=[]
+        origSizeHand=sizeHand
+        while sizeHand>0 and len(deck)>0:
+            hand.append(deck.pop())
+            sizeHand-=1
+        sizeHand=origSizeHand
+        hands.append(hand)
+        numHands-=1
+    return hands
 
 myDeck=shuffle_deck(build_deck())
 myFirstCard=deal_top_card(myDeck)
-print(myFirstCard)
+myHands=deal_hands(myDeck,2,5)
+print(myHands)
